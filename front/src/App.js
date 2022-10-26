399ea8ca4ec6de8a3603d6dd455435b7be2d80cb
jche0171 <jche0171@student.monash.edu>
2022-10-27 00:43:50 +1100

    images

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import images from "./api-mock.json";
+import "./App.css";
 
 const App = () => {
-  return <div>Hello !!!!</div>;
+  const [imageList, setImageList] = useState(images.photos.photo);
+
+  return (
+    <div className="image-grid">
+      {imageList.map((image) => (
+        <img
+          src={
+            "https://live.staticflickr.com/" +
+            image.server +
+            "/" +
+            image.id +
+            "_" +
+            image.secret +
+            "_w.jpg"
+          }
+          alt={image.title}
+        ></img>
+      ))}
+    </div>
+  );
 };
 
 export default App;
